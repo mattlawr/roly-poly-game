13f7ec9dae1bfebbae730236cd8c261ee4229e5a
Matthew Lawrence <mattblawr@gmail.com>
2020-02-01 08:49:42 -0800

    First commit

---
+++ Roly Poly/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Entity
+{
+    public float speed = 2f;
+    public LayerMask groundLayers;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

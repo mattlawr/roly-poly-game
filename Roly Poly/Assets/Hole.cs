cc4b1eff8b20d273f3793283b12693f238b56db4
dodecamon <44084222+dodecamon@users.noreply.github.com>
2020-02-01 13:54:45 -0800

    Above the ground - basic

---
+++ Roly Poly/Assets/Hole.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    public GameObject Button;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Button.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Button.SetActive(false);
+        }
+    }
+}
---
+++ Roly Poly/Assets/Scripts/jiaqi's scripts/CharacterController2D.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// A simple controller for animating a 4 directional sprite using Physics.
+/// </summary>
+public class CharacterController2D : MonoBehaviour
+{
+    public float speed = 100;
+    Rigidbody2D rb;
+    public bool isMoving = false;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if (h != 0 || v != 0)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+
+        Vector3 tempVect = new Vector3(h, v, 0);
+        tempVect = tempVect * speed * Time.deltaTime;
+        rb.MovePosition(rb.transform.position + tempVect);
+    }
+}

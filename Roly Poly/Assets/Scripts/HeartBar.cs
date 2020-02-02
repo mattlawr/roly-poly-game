25d5f02a33c0b2dd5593092be3223a23f914ba2b
Matthew Lawrence <mattblawr@gmail.com>
2020-02-02 13:48:38 -0800

    Goop explode

--- Roly Poly/Assets/Scripts/HeartBar.cs
+++ Roly Poly/Assets/Scripts/HeartBar.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class HeartBar : MonoBehaviour
 {
+    public int hearts = 10;
+
+    private SpriteRenderer[] heartSprites;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (true)
+        {
+            heartSprites = new SpriteRenderer[transform.childCount];
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                if (!transform.GetChild(i).GetComponent<SpriteRenderer>())
+                {
+                    Debug.LogError("No hearts!");
+                    break;
+                }
+
+                heartSprites[i] = transform.GetChild(i).GetComponent<SpriteRenderer>();
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //UpdateHearts(0);
+    }
+
+    public void UpdateHearts(int d)
+    {
+        hearts += d;
+        if(hearts > heartSprites.Length) { hearts = heartSprites.Length; }
+
+        for (int i = 0; i < heartSprites.Length; i++)
+        {
+            if(i < hearts)
+            {
+                heartSprites[i].color = Color.red;
+            } else
+            {
+                heartSprites[i].color = Color.black;
+            }
+        }
     }
 }

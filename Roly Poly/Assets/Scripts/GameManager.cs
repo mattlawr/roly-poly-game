2c919cd1204b5d1d225915a6d6fcef4a870053ef
Matthew Lawrence <mattblawr@gmail.com>
2020-02-01 18:20:23 -0800

    level grid

--- Roly Poly/Assets/Scripts/GameManager.cs
+++ Roly Poly/Assets/Scripts/GameManager.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public Player rolySidePrefab;
+    private Player roly;
+
     public Transform levelsParent;
     private Level[] levels;
 
     private Transform worldMap;
+    private Vector3 firstPos;
 
     public static GameManager instance = null;
 
@@ -31,6 +34,8 @@ public class GameManager : MonoBehaviour
     {
         worldMap = transform.GetChild(0);// First child
 
+        firstPos = transform.position;
+
         if (levelsParent)
         {
             levels = new Level[levelsParent.childCount];
@@ -50,18 +55,39 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (roly)
+        {
+            float diff = roly.transform.position.y - transform.position.y;
+
+            Vector3 follow = transform.position;
+
+            if(diff > 3f || diff < -2f)
+            {
+                follow += new Vector3(0, diff/10f);
+            }
+
+            transform.position = follow;
+        }
+    }
+
+    void ResetCamera()
+    {
+        transform.position = firstPos;
     }
 
     public void EnterLevel(int id)
     {
         worldMap.gameObject.SetActive(false);
 
-        levels[id].StartLevel(rolySidePrefab);
+        ResetCamera();
+
+        roly = levels[id].StartLevel(rolySidePrefab);
     }
 
     public void ExitLevel()
     {
+        ResetCamera();
+
         worldMap.gameObject.SetActive(true);
 
     }
